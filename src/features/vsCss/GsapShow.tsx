0a2d7bb1bf21d0dae9d0641ab895c8201260e21a
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2026-07-21 11:11:06 +0300

    feat: add vsCss

---
+++ src/features/vsCss/GsapShow.tsx
@@ -0,0 +1,63 @@
+import { useSlideAnimation } from "@/hooks/useSlideAnimation";
+import { useRef } from "react";
+import { animations } from "../Basics/constants";
+
+function GsapShow() {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useSlideAnimation(containerRef, animations);
+
+  return (
+    <section className="section-padding h-dvh" ref={containerRef}>
+      <div className="bg-primary text-accent-foreground h-full section-content py-16 px-12">
+        <h2
+          className="text-6xl font-bold uppercase mb-14"
+          data-animation="step"
+        >
+          CSS vs GSAP
+        </h2>
+        <p className="text-3xl leading-relaxed mb-12" data-animation="step">
+          It's not about replacing CSS, it's about choosing the right tool.
+        </p>
+        <h3 className="text-4xl font-bold mb-6" data-animation="step">
+          GSAP
+        </h3>
+        <div className="grid grid-cols-2">
+          <div className="space-y-2">
+            <p
+              className="text-3xl border-b-3 border-dashed w-max"
+              data-animation="step"
+            >
+              Best for:
+            </p>
+            <ul
+              className="list-disc list-inside text-3xl space-y-3"
+              data-animation="step"
+            >
+              <li data-animation="step">Complex timelines ✅</li>
+              <li data-animation="step">Scroll-driven experiences ✅</li>
+              <li data-animation="step">Powerful animation control ✅</li>
+            </ul>
+          </div>
+          <div className="space-y-2">
+            <p
+              className="text-3xl border-b-3 border-dashed w-max"
+              data-animation="step"
+            >
+              Limitations:
+            </p>
+            <ul
+              className="list-disc list-inside text-3xl space-y-3"
+              data-animation="step"
+            >
+              <li data-animation="step">Additional dependency ❌</li>
+              <li data-animation="step">Slightly steeper learning curve ❌</li>
+              <li data-animation="step">Performance overhead ❌</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+export default GsapShow;
---
+++ src/features/vsCss/vsCss.tsx
@@ -0,0 +1,12 @@
+import GsapShow from "./GsapShow";
+import CssShow from "./CssShow";
+
+function VsCss() {
+  return (
+    <>
+      <CssShow />
+      <GsapShow />
+    </>
+  );
+}
+export default VsCss;

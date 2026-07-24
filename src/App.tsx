5c33446560b773ec06ebfd65477d0a9ea1038465
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2026-07-24 16:18:59 +0300

    feat: prevent right click on slides

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
+import { useEffect } from "react";
 import Home from "./pages/Home";
 
 const App = () => {
+  useEffect(() => {
+    const handleContextMenu = (e: MouseEvent) => {
+      e.preventDefault();
+    };
+
+    document.addEventListener("contextmenu", handleContextMenu);
+
+    return () => {
+      document.removeEventListener("contextmenu", handleContextMenu);
+    };
+  }, []);
+
   return <Home />;
 };
 
